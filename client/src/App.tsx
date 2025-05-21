063ea98d236b19d9fc4da8e636a25c6406426caf
AnonDrew <segdrew328@gmail.com>
2025-05-21 09:46:36 -0400

    Add initial state and fetch function for todos in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [title, setTitle] = useState("");
+
+  const url = "http://localhost:5000/tasks";
+
+  const getTodos = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setTodos(data);
+  };
+
   return (
     <>
       Hello world!
