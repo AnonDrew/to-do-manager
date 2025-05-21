30973f75cbea7c5ff75c8c6c778d15a3e9c71972
AnonDrew <segdrew328@gmail.com>
2025-05-21 09:37:06 -0400

    Implement CRUD operations for todos in the API

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,3 +14,33 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+app.get("/todos", async (_, res) => {
+  const q = "SELECT * FROM tasks ORDER BY id";
+  const result = await pool.query(q);
+  res.json(result.rows);
+});
+
+app.post("/tasks", async (req, res) => {
+  const { title } = req.body;
+  const result = await pool.query(
+    "INSERT INTO tasks (title) VALUES ($1) RETURNING *",
+    [title]
+  );
+  res.status(201).json(result.rows[0])
+});
+
+app.put("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  const { completed } = req.body;
+  const result = await pool.query(
+    "UPDATE tasks SET completed = $1 WHERE id = $2 RETURNING *",
+    [completed, id]
+  );
+  res.json(result.rows[0]);
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  await pool.query("DELETE FROM tasks WHERE id = $1", [id]);
+  res.sendStatus(204);
+});
