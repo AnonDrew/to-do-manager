e6cfe9e15a5ed1408902e39d0b01d9f43beb15a6
AnonDrew <segdrew328@gmail.com>
2025-05-21 09:15:30 -0400

    Init Express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,3 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { Pool } from "pg";
 
+dotenv.config()
+
+const app = express()
+const port = 5000;
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
